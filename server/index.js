3fbc13d47cce66d0bc9ba089ed0d602f0adaf3f9
wender103 <wendertestes01@gmail.com>
2024-01-08 18:24:13 -0300

    ;

--- server/index.js
+++ server/index.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); // Permitir solicitações de qualquer origem
+// Permitir solicitações de qualquer origem (CORS)
+app.use(cors({ origin: '*' }));
 app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/ligar-pc', (req, res) => {
-        //Meu pc 'AC-22-0B-2E-13-5C'
+    //Meu pc 'AC-22-0B-2E-13-5C'
     //Not '80-EE-73-0D-AA-09'
-    const macAddress = req.body.macAddress || '80:EE:73:0D:AA:09'; // Pega o endereço MAC do corpo da requisição ou usa um padrão
-    
-    // Envia o comando Wake-on-LAN
+    const macAddress = req.body.macAddress || '80:EE:73:0D:AA:09';
+
     wakeonlan.wake(macAddress, (err) => {
         if (err) {
             console.error('Erro ao enviar o comando Wake-on-LAN:', err);
